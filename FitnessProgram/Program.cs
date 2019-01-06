79cb07e64cc1d199fc44a88e0bb08f522d702246
Robert Glover <robert_f_g@hotmail.com>
2019-01-05 17:32:18 -0800

    Added namespace FitnessTracker

--- FitnessProgram/Program.cs
+++ FitnessProgram/Program.cs
@@ -1,21 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FitnessProgram
+namespace FitnessTracker
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Prompt the user for minutes exercised
+            System.Console.Write("Enter the number of minutes you have exercised:");
+            
+            // Add minutes exercised to total
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Display total minutes exercised on screen
+
+            // Repeat until user quits
+
         }
     }
 }
